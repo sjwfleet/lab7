ce31eaefbc6233b7e5012d749e1d51fa099c9a1d
Samuel Fleet <sfleet@ucsd.edu>
2021-02-16 18:30:22 -0800

    Lab7

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -9,5 +9,12 @@ $(document).ready(function() {
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	// your code here
+	$('.likeCtr').click(GOOGLE);
+}
+
+function GOOGLE(e) {
+	// Prevent following the link
+	e.preventDefault();
+
+	ga("send", "event", 'like', 'click');
 }
